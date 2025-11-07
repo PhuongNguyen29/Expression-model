483511074c75079f42cf5c139f74be52e5666516
PhuongNguyen29 <nguyenphuongyds@gmail.com>
2025-11-07 01:13:15 -0600

    Phase 1: Alpha parameter investigation_group lasso

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -4,6 +4,7 @@ from src.utils.regularization import (
     l1_penalty,
     l2_penalty,
     elastic_net_penalty,
+    group_lasso_penalty,
     get_feature_importance,
     count_zero_weights
 )
@@ -17,6 +18,7 @@ __all__ = [
     'l1_penalty',
     'l2_penalty',
     'elastic_net_penalty',
+    'group_lasso_penalty',
     'get_feature_importance',
     'count_zero_weights',
     'StratifiedBatchSampler',
