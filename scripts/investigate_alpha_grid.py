3fc2a371aa58da33051610e2ce9586603e5ac34a
PhuongNguyen29 <nguyenphuongyds@gmail.com>
2025-11-07 23:47:09 -0600

    Phase 1: Alpha parameter investigation_group lasso_full data_0.0005_0.7

--- scripts/investigate_alpha_grid.py
+++ scripts/investigate_alpha_grid.py
@@ -132,8 +132,8 @@ class AlphaInvestigator:
         # Alpha values to test (logarithmically spaced)
         # Based on Cox elastic net literature (Simon et al. 2011)
         # self.alpha_values = [0.001, 0.01, 0.05, 0.1, 0.5, 1.0, 2.0, 5.0]
-        self.lambda_values = [0.00005, 0.0001, 0.0005, 0.001, 0.002, 0.003]
-        self.l1_ratio_values = [0.3, 0.5, 0.7, 0.9, 1.0]
+        self.lambda_values = [0.0005]
+        self.l1_ratio_values = [0.7]
         self.alpha_values = self.lambda_values
         
         logger.info(f"Testing {len(self.lambda_values)} lambda values: {self.lambda_values}")
