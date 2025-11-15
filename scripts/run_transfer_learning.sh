8cf902ec0bbe9e71a11664d7559a5a5a20f6de80
PhuongNguyen29 <nguyenphuongyds@gmail.com>
2025-11-15 00:12:01 -0600

    Phase4: Transfer step ORIEN-> TCGA (seed 42)

--- scripts/run_transfer_learning.sh
+++ scripts/run_transfer_learning.sh
@@ -4,11 +4,21 @@
 export PYTHONPATH="${PWD}:${PYTHONPATH}"
 
 # Run transfer learning: ORIEN → TCGA
+# python scripts/transfer_learning_trainer.py \
+#     --source_cohort orien \
+#     --target_cohort tcga \
+#     --source_params results/hyperparam_FIXED_orien_20251109_195430/best_params.json \
+#     --target_params results/hyperparam_FIXED_tcga_20251109_194909/best_params.json \
+#     --seed 42 \
+#     --device cuda \
+#     2>&1 | tee logs/transfer_learning_seed42.log
+
+
 python scripts/transfer_learning_trainer.py \
-    --source_cohort orien \
-    --target_cohort tcga \
-    --source_params results/hyperparam_FIXED_orien_20251109_195430/best_params.json \
-    --target_params results/hyperparam_FIXED_tcga_20251109_194909/best_params.json \
-    --seed 42 \
+    --source_cohort tcga \
+    --target_cohort orien \
+    --source_params results/hyperparam_FIXED_tcga_20251109_194909/best_params.json \
+    --target_params results/hyperparam_FIXED_orien_20251109_195430/best_params.json \
+    --seed 42\
     --device cuda \
-    2>&1 | tee logs/transfer_learning_seed42.log
+    2>&1 | tee logs/transfer_learning_seed42_tcga_to_orien.log
