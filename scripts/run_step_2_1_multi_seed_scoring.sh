fac94307ea125835ccd6a6b73b2d76a9829df2a8
PhuongNguyen29 <nguyenphuongyds@gmail.com>
2025-11-17 18:27:05 -0600

    Step2.1: impaotence_scoring

--- scripts/run_step_2_1_multi_seed_scoring.sh
+++ scripts/run_step_2_1_multi_seed_scoring.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PYTHONPATH="${PWD}:${PYTHONPATH}"
 
-python extract_biomarkers_from_best_params.py \
+python scripts/extract_biomarkers_from_best_params.py \
   --tcga_params results_v2/01_hyperparameter_tuning/tcga_308genes/best_params.json \
   --orien_params results_v2/01_hyperparameter_tuning/orien_308genes/best_params.json \
   --consensus_genes data/processed/consensus_genes_308.txt \
