f2af0bbea37006df334e191ce441f7ef21c2ac56
PhuongNguyen29 <nguyenphuongyds@gmail.com>
2025-11-27 18:13:58 -0600

    Step3.1:baseline target only

--- scripts/step3_3_finetune.py
+++ scripts/step3_3_finetune.py
@@ -181,7 +181,7 @@ def get_finetune_config(direction, source_params_file, target_params_file):
         target_batch_size = target_params['batch_size']  # ORIEN's batch size
     
     # Fine-tuning LR = source LR / 10
-    finetune_lr = source_lr / 2
+    finetune_lr = source_lr * 0.75
     
     return target_cohort, finetune_lr, target_batch_size
 
